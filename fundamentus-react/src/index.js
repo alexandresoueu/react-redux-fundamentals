f8fba5900d2330444c49e8d07ac8d33d74afdb85
calexandredev <calexandredev@gmail.com>
2020-07-18 17:17:30 -0300

    greeting and css

---
+++ fundamentus-react/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import './index.css'
+
+const greeting = <strong>HELLO PIRIQUITO</strong>
+
+ReactDOM.render(
+<div>{greeting}</div>,
+  document.getElementById('root')
+)
