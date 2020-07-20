b4cf3312b1d4c492f0176e3fd473eb890f577f48
calexandredev <calexandredev@gmail.com>
2020-07-20 20:53:00 -0300

    using jsx into the component

--- fundamentus-react/src/components/basics/FirstComponent.js
+++ fundamentus-react/src/components/basics/FirstComponent.js
@@ -1,5 +1,13 @@
+import React from 'react'
+
 function FirstComponent() {
-  return 'The First Component'
+  const message = 'Hey dude...'
+  return (
+    <div>
+      <h2>First Component</h2>
+      <p>{ message }</p>
+    </div>
+  )
 }
 
 export default FirstComponent
