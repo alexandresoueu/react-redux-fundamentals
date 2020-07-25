c917227f7d2ab29d10ca4709c84403bf4263a107
calexandredev <calexandredev@gmail.com>
2020-07-24 22:45:06 -0300

    create component cards

--- fundamentus-react/src/App.js
+++ fundamentus-react/src/App.js
@@ -4,13 +4,19 @@ import './index.css'
 import FirstComponent from './components/basics/FirstComponent'
 import ComponentWithParams from './components/basics/ComponentWithParams'
 import Random from './components/basics/Random'
+import Card from './components/layout/Card'
 
 function App(props) {
   return (
     <div id="app">
-      <FirstComponent />,
-      <ComponentWithParams title='Nemoo' subtitle='Bulbassauro' />,
-      <Random min={1} max={60} />
+      <FirstComponent />
+      <Card title="Component Dad">
+        <Random min={1} max={60} />
+      </Card>
+      <Card title="Component with Params">
+        <ComponentWithParams title='Nemoo' subtitle='Bulbassauro' score={9.3} />
+      </Card>
+      
     </div>
   )
 }
--- fundamentus-react/src/components/basics/ComponentWithParams.js
+++ fundamentus-react/src/components/basics/ComponentWithParams.js
@@ -5,6 +5,7 @@ function ComponentWithParams(props) {
     <div>
       <h2>{props.title}</h2>
       <h3>{props.subtitle}</h3>
+      <h4>{props.score}</h4>
     </div>
   )
 }
