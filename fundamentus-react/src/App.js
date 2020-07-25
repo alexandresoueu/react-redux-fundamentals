8d6b639c58da7cbf696846450e7ce89c2cc2a9bc
calexandredev <calexandredev@gmail.com>
2020-07-25 12:09:53 -0300

    Using Flexbox to do responsive layout

--- fundamentus-react/src/App.js
+++ fundamentus-react/src/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
 
-import './index.css'
 import FirstComponent from './components/basics/FirstComponent'
 import ComponentWithParams from './components/basics/ComponentWithParams'
 import Random from './components/basics/Random'
 import Card from './components/layout/Card'
 
+import './App.css'
+
 function App(props) {
   return (
-    <div id="app">
+    <div className="app">
       <FirstComponent />
-      <Card title="Component Dad">
-        <Random min={1} max={60} />
-      </Card>
-      <Card title="Component with Params">
-        <ComponentWithParams title='Nemoo' subtitle='Bulbassauro' score={9.3} />
-      </Card>
-      
+      <div className="cards">
+        <Card title="Component Dad">
+          <Random min={1} max={60} />
+        </Card>
+        <Card title="Component with Params">
+          <ComponentWithParams title='Nemoo' subtitle='Bulbassauro' score={9.3} />
+        </Card>
+      </div>
+
     </div>
   )
 }
