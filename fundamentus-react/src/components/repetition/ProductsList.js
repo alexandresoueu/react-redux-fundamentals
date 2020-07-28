4ee545133c9dfbe8143b9890bb86957898972a85
calexandredev <calexandredev@gmail.com>
2020-07-28 16:19:50 -0300

    Add css on table

--- fundamentus-react/src/components/repetition/ProductsList.js
+++ fundamentus-react/src/components/repetition/ProductsList.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import products from '../../data/products'
+import './ProductsList.css'
 
-function ProductsList (props) {
-  const listProducts = products.map(item => {
-    return <tr key={item.id}>
+function ProductsList(props) {
+  const listProducts = products.map((item, i) => {
+    return <tr key={item.id} className={i % 2 === 0 ? 'Pair' : 'Odd'}>
       <td>{item.id}</td>
       <td>{item.name}</td>
       <td>R$ {item.price.toFixed(2)}</td>
     </tr>
   })
 
-  return (<table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-    <tbody>
-      {listProducts}
-    </tbody>
-  </table>
+  return (<div className='ProductsTable'>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listProducts}
+      </tbody>
+    </table>
+  </div>
   )
 }
 
